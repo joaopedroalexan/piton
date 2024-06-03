7e924d5415bce8cf06de3280cb751cf8647673cc
Joao_alex <joaopedroalexsilva@gmail.com>
2024-06-03 16:22:58 -0300

    commit daora

--- aplicacoes.py
+++ aplicacoes.py
@@ -1,40 +1,83 @@
 import time
 
-lista = [17,3,11,5,1,9,7,15,18]
+# Exemplo 1
+lista = [17, 3, 11, 5, 1, 9, 7, 15, 18]
 maior_numero = lista[0]
 
-for i in range(1, len(lista)):
-    if lista[i] > maior_numero:
-        maior_numero = lista[i]
-print("O Maior numero da lista é ", maior_numero)
+for numero in lista:
+    if numero > maior_numero:
+        maior_numero = numero
 
-#Exemplo2
-my_list = [17,3,11,5,1,9,7,15,18]
+print("O Maior número da lista é ", maior_numero)
+
+# Exemplo 2
+my_list = [17, 3, 11, 5, 1, 9, 7, 15, 18]
 maior = my_list[0]
-for i in my_list:
-    if i > maior:
-        maior = i
-print("Maior valor 2:", maior)
 
-#Exemplo3:
+for numero in my_list:
+    if numero > maior:
+        maior = numero
+
+print("Maior valor:", maior)
+
+# Exemplo 3
 ultima_lista = my_list[:]
 mel = ultima_lista[0]
-for i in ultima_lista[1:]:
-    if i > mel:
-        mel = i
-print("maior valor 3: ", mel)
 
-frutas = ["abacaxi","maça","pera","mamao","uva","melancia"]
+for numero in ultima_lista[1:]:
+    if numero > mel:
+        mel = numero
+
+print("Maior valor:", mel)
+
+# Exemplo 4
+frutas = ["abacaxi", "maça", "pera", "mamao", "uva", "melancia"]
 elemento = "melancia"
 achado = False
 
 for i in range(len(frutas)):
-    achado = frutas[i] == elemento
-    if achado:
+    if frutas[i] == elemento:
+        achado = True
         break
 
 if achado:
-    print("elemento encontrado no indice ", i)
+    print("Elemento encontrado no índice", i)
 else:
     print("NÃO ENCONTRADO!!!")
-time.sleep
+
+# Exemplo 5
+sorteados = [5, 11, 9, 42, 3, 49]
+apostados = [3, 7, 11, 42, 34, 49]
+acertos = 0
+
+for numero in apostados:
+    if numero in sorteados:
+        acertos += 1
+
+print("Número de acertos:", acertos)
+
+# Exemplo 6
+lista = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+print("Lista original:", lista)
+
+# Lista de apoio
+vistos = []
+
+# Iterar
+for numero in lista[::-1]:  # Inverter a lista para percorrer do fim ao início
+    if numero in vistos:
+        lista.remove(numero)
+    else:
+        vistos.append(numero)
+
+print("Lista modificada:", lista)
+
+# Matriz tridimensional
+cubo = [[["1,2,3"], ["2,3,1"], ["3,2,1"]],[["4,5,6"], ["5,6,4"], ["6,5,4"]],[["7","8","9"],["8","9","7"],["9","8","7"]]]
+
+print(cubo)
+print(cubo[1])
+print(cubo[1],[0])
+print(cubo[1],[0],[2])
+
+time.sleep(1)
